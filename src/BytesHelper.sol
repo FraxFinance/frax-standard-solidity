37d4d9854ad17d8fc0732b905820509ead5afacf
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2023-06-15 10:46:43 -0400

    feat: BytesHelper

---
+++ src/BytesHelper.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.19;
+
+import { BytesLib } from "solidity-bytes-utils/contracts/BytesLib.sol";
+
+library BytesHelper {
+    function concat(bytes memory _preBytes, bytes memory _postBytes) internal pure returns (bytes memory) {
+        return BytesLib.concat(_preBytes, _postBytes);
+    }
+
+    function concatStorage(bytes storage _preBytes, bytes memory _postBytes) internal {
+        return BytesLib.concatStorage(_preBytes, _postBytes);
+    }
+
+    function slice(bytes memory _bytes, uint256 _start, uint256 _length) internal pure returns (bytes memory) {
+        return BytesLib.slice(_bytes, _start, _length);
+    }
+
+    function toAddress(bytes memory _bytes, uint256 _start) internal pure returns (address) {
+        return BytesLib.toAddress(_bytes, _start);
+    }
+
+    function toUint256(bytes memory _bytes, uint256 _start) internal pure returns (uint256) {
+        return BytesLib.toUint256(_bytes, _start);
+    }
+
+    function toBytes32(bytes memory _bytes, uint256 _start) internal pure returns (bytes32) {
+        return BytesLib.toBytes32(_bytes, _start);
+    }
+
+    function equal(bytes memory _preBytes, bytes memory _postBytes) internal pure returns (bool) {
+        return BytesLib.equal(_preBytes, _postBytes);
+    }
+
+    function equalStorage(bytes storage _preBytes, bytes memory _postBytes) internal view returns (bool) {
+        return BytesLib.equalStorage(_preBytes, _postBytes);
+    }
+}
