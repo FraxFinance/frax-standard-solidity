4b272e69525b4112f7dd6b32b8c2b7da9b26d980
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2023-06-14 22:15:47 -0400

    feat: extend the concat function on array helper

--- src/ArrayHelper.sol
+++ src/ArrayHelper.sol
@@ -7,4 +7,25 @@ library ArrayHelper {
         }
         _newArray[_inputArrayLength] = _newItem;
     }
+
+    function concat(bytes32[] memory _inputArray, bytes32 _newItem) internal pure returns (bytes32[] memory _newArray) {
+        uint256 _inputArrayLength = _inputArray.length;
+        _newArray = new bytes32[](_inputArrayLength + 1);
+        for (uint256 i = 0; i < _inputArrayLength; i++) {
+            _newArray[i] = _inputArray[i];
+        }
+        _newArray[_inputArrayLength] = _newItem;
+    }
+
+    function concat(
+        bytes[] memory _inputArray,
+        bytes memory _newItem
+    ) internal pure returns (bytes[] memory _newArray) {
+        uint256 _inputArrayLength = _inputArray.length;
+        _newArray = new bytes[](_inputArrayLength + 1);
+        for (uint256 i = 0; i < _inputArrayLength; i++) {
+            _newArray[i] = _inputArray[i];
+        }
+        _newArray[_inputArrayLength] = _newItem;
+    }
 }
