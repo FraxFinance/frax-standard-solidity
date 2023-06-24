3e9efcfd4241c5ad1d8e751aa4493e883edbc61a
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2023-06-24 11:49:45 -0400

    feat: extend ArrayHelper

--- src/ArrayHelper.sol
+++ src/ArrayHelper.sol
@@ -8,6 +8,33 @@ library ArrayHelper {
         _newArray[_inputArrayLength] = _newItem;
     }
 
+    function concat(bool[] memory _inputArray, bool _newItem) internal pure returns (bool[] memory _newArray) {
+        uint256 _inputArrayLength = _inputArray.length;
+        _newArray = new bool[](_inputArrayLength + 1);
+        for (uint256 i = 0; i < _inputArrayLength; i++) {
+            _newArray[i] = _inputArray[i];
+        }
+        _newArray[_inputArrayLength] = _newItem;
+    }
+
+    function concat(uint128[] memory _inputArray, uint128 _newItem) internal pure returns (uint128[] memory _newArray) {
+        uint256 _inputArrayLength = _inputArray.length;
+        _newArray = new uint128[](_inputArrayLength + 1);
+        for (uint256 i = 0; i < _inputArrayLength; i++) {
+            _newArray[i] = _inputArray[i];
+        }
+        _newArray[_inputArrayLength] = _newItem;
+    }
+
+    function concat(uint32[] memory _inputArray, uint32 _newItem) internal pure returns (uint32[] memory _newArray) {
+        uint256 _inputArrayLength = _inputArray.length;
+        _newArray = new uint32[](_inputArrayLength + 1);
+        for (uint256 i = 0; i < _inputArrayLength; i++) {
+            _newArray[i] = _inputArray[i];
+        }
+        _newArray[_inputArrayLength] = _newItem;
+    }
+
     function concat(bytes32[] memory _inputArray, bytes32 _newItem) internal pure returns (bytes32[] memory _newArray) {
         uint256 _inputArrayLength = _inputArray.length;
         _newArray = new bytes32[](_inputArrayLength + 1);
@@ -23,6 +50,15 @@ library ArrayHelper {
     ) internal pure returns (bytes[] memory _newArray) {
         uint256 _inputArrayLength = _inputArray.length;
         _newArray = new bytes[](_inputArrayLength + 1);
+        for (uint256 i = 0; i < _inputArrayLength; i++) {
+            _newArray[i] = _inputArray[i];
+        }
+        _newArray[_inputArrayLength] = _newItem;
+    }
+
+    function concat(uint256[] memory _inputArray, uint256 _newItem) internal pure returns (uint256[] memory _newArray) {
+        uint256 _inputArrayLength = _inputArray.length;
+        _newArray = new uint256[](_inputArrayLength + 1);
         for (uint256 i = 0; i < _inputArrayLength; i++) {
             _newArray[i] = _inputArray[i];
         }
