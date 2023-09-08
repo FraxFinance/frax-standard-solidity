77b38ec17e764cb48f10525c751ec0303a1c0886
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2023-09-08 16:13:02 -0400

    feat: add pragma to ArrayHelper

--- src/ArrayHelper.sol
+++ src/ArrayHelper.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: ISC
+pragma solidity >=0.8.0;
+
 library ArrayHelper {
     function concat(address[] memory _inputArray, address _newItem) internal pure returns (address[] memory _newArray) {
         uint256 _inputArrayLength = _inputArray.length;
