dad99259d6847862b030156779d7b31ed8412659
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2023-09-09 20:59:30 -0400

    feat: add mineBlocksToTimestamp

--- src/VmHelper.sol
+++ src/VmHelper.sol
@@ -3,14 +3,14 @@ pragma solidity >=0.8.0;
 import { CommonBase } from "forge-std/Base.sol";
 
 contract VmHelper is CommonBase {
-    struct MineOneBlockResult {
+    struct MineBlocksResult {
         uint256 timeElapsed;
         uint256 blocksElapsed;
         uint256 currentTimestamp;
         uint256 currentBlockNumber;
     }
 
-    function mineOneBlock() public returns (MineOneBlockResult memory result) {
+    function mineOneBlock() public returns (MineBlocksResult memory result) {
         uint256 timeElapsed = 12;
         uint256 blocksElapsed = 1;
         vm.warp(block.timestamp + timeElapsed);
@@ -19,13 +19,6 @@ contract VmHelper is CommonBase {
         result.blocksElapsed = blocksElapsed;
         result.currentTimestamp = block.timestamp;
         result.currentBlockNumber = block.number;
-    }
-
-    struct MineBlocksResult {
-        uint256 timeElapsed;
-        uint256 blocksElapsed;
-        uint256 currentTimestamp;
-        uint256 currentBlockNumber;
     }
 
     // helper to move forward multiple blocks
@@ -41,17 +34,22 @@ contract VmHelper is CommonBase {
         result.currentBlockNumber = block.number;
     }
 
-    struct MineBlocksBySecondResult {
-        uint256 timeElapsed;
-        uint256 blocksElapsed;
-        uint256 currentTimestamp;
-        uint256 currentBlockNumber;
-    }
-
-    function mineBlocksBySecond(uint256 secondsElapsed) public returns (MineBlocksBySecondResult memory result) {
+    function mineBlocksBySecond(uint256 secondsElapsed) public returns (MineBlocksResult memory result) {
         uint256 timeElapsed = secondsElapsed;
         uint256 blocksElapsed = secondsElapsed / 12;
         vm.warp(block.timestamp + timeElapsed);
+        vm.roll(block.number + blocksElapsed);
+
+        result.timeElapsed = timeElapsed;
+        result.blocksElapsed = blocksElapsed;
+        result.currentTimestamp = block.timestamp;
+        result.currentBlockNumber = block.number;
+    }
+
+    function mineBlocksToTimestamp(uint256 _timestamp) public returns (MineBlocksResult memory result) {
+        uint256 timeElapsed = _timestamp - block.timestamp;
+        uint256 blocksElapsed = timeElapsed / 12;
+        vm.warp(_timestamp);
         vm.roll(block.number + blocksElapsed);
 
         result.timeElapsed = timeElapsed;
