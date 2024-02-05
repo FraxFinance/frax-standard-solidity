ad151a9003e877110dee48bce4d59eeed33ce242
Thomas Clement <thomasaclement98@gmail.com>
2024-02-05 15:27:14 -0500

    Fix
    Reverting to the first snapshot in the list, also implicitly resets the array of snapShotIds to Only have one member

---
+++ test/TestMultipleSetup.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: ISC
+pragma solidity >=0.8.0;
+
+import "../src/FraxTest.sol";
+import { ArrayHelper } from "../src/ArrayHelper.sol";
+
+contract TestMultipleSetup is FraxTest {
+    uint256 value;
+
+    function initializeValueOne() public {
+        value = 25;
+    }
+
+    function initializeValueTwo() public {
+        value = 5;
+    }
+
+    function initializeValueThree() public {
+        value = 10;
+    }
+
+    function setUp() public {
+        setupFunctions.push(initializeValueOne);
+        setupFunctions.push(initializeValueTwo);
+        setupFunctions.push(initializeValueThree);
+        addSetupFunctions(setupFunctions);
+    }
+
+    function testFailAssertValue() public useMultipleSetupFunctions {
+        assertEq(value, 5);
+    }
+}
