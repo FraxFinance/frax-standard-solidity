c951978a3fbcbcfe9ed65c9e99e420facdee772f
JP <89362795+0xJayPi@users.noreply.github.com>
2024-08-10 00:12:38 +0200

    `FraxTest` Coverage (#23)
    * Initial commit
    
    * Coverage added for unpackBitsAndLogUin(bytes32,uint256,uint256)
    
    * Coverage added for unpackBitsAndLogUin(uint256,uint256,uint256)
    
    * Removed package-lock.json and updated .gitignore accordingly
    
    * Removed package-lock.json
    
    * Reverted foundry.toml unexpected update

--- test/TestSlotDump.t.sol
+++ test/TestSlotDump.t.sol
@@ -11,13 +11,30 @@ contract TestSlotDump is FraxTest {
         dumpStorageLayout(instance, 15);
     }
 
-    function testUnpackSlot() public {
+    function testUnpackSlotBytes() public {
         instance = address(new Charlie());
         dumpStorageLayout(instance, 15);
 
         bytes32 packedSlot = vm.load(address(instance), bytes32(uint(9)));
-        uint256 unpacked1 = unpackBits(packedSlot, 0, 96);
-        uint256 unpacked2 = unpackBits(packedSlot, 96, 160);
+        uint256 unpacked1 = unpackBitsAndLogUint(packedSlot, 0, 96);
+        uint256 unpacked2 = unpackBitsAndLogUint(packedSlot, 96, 160);
+
+        /// @notice `unpacked1` is `uint96` expressed as `uint256`
+        assertEq(22222222222222222222, unpacked1);
+        assertEq(22222222222222222222, uint96(unpacked1));
+
+        /// @notice `unpacked2` is `address` expressed as `uint256`
+        assertEq(uint256(uint160(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)), unpacked2);
+        assertEq(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE, address(uint160(unpacked2)));
+    }
+
+    function testUnpackSlotUint() public {
+        instance = address(new Charlie());
+        dumpStorageLayout(instance, 15);
+
+        uint256 packedSlot = uint256(vm.load(address(instance), bytes32(uint(9))));
+        uint256 unpacked1 = unpackBitsAndLogUint(packedSlot, 0, 96);
+        uint256 unpacked2 = unpackBitsAndLogUint(packedSlot, 96, 160);
 
         /// @notice `unpacked1` is `uint96` expressed as `uint256`
         assertEq(22222222222222222222, unpacked1);
